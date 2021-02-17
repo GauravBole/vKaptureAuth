62f33878e3cc33dd2726e97c4f6945e1ceaab349
gauravbole <gauravbole94@gmail.com>
2021-02-17 23:06:38 +0530

    video add functinality

--- auth/dao/photographer_profile.py
+++ auth/dao/photographer_profile.py
@@ -12,3 +12,11 @@ class PhotographerProfileDao:
             # print(e, "--->")
             raise DaoExceptionError(status_code=401, message="Error in inquiry creation dao", detal_message=e)
             
+    def upload_video(self, video_path:str, user:int, cursor=None):
+        try:
+            add_video_query = f"insert into portfolio_video (video_path, created_by_id, source) values ('{video_path}', {user}, 'file')"
+            cursor.execute(add_video_query)
+        except Exception as e:
+            # print(e, "--->")
+            raise DaoExceptionError(status_code=401, message="Error in inquiry creation dao", detal_message=e)
+    
--- auth/views/photographer_profile.py
+++ auth/views/photographer_profile.py
@@ -38,15 +38,52 @@ class PhotographerPortfolioImagesApiView(MethodView):
 
         return make_response(jsonify(response_data)), response_data['status_code']
 
+
+class PhotographerPortfolioVideoApiView(MethodView):
+    decorators = [authanticate]
+
+    def post(self):
+        response_data = {"message": {'msg': "something wents wrong"}, "status": "fail", "status_code": 501}
+        try:
+            if request.files:
+                post_data = request.files['video']
+                photographer_image_service = PhotographerPortfolio()
+                user = request.environ['user']
+                user_id = user['user_id']
+                photographer_image_service.upload_video(video=post_data, user_id=user_id)
+
+                # long_task = threading.Thread(target=photographer_image_service.upload_image, kwargs={"image":post_data, "user_id":user_id})
+                # # task = threading.Thread(photographer_image_service.upload_image, kwargs={"image":post_data, "user_id":user_id)]
+                # long_task.start()
+
+                response_data['message'] = {'msg': "file uloaded successfully"},
+                response_data["status"] = "success"
+                response_data["status_code"] =  200
         
+            
+        except (DaoExceptionError, ExceptionError) as ex:
+            response_data['message'] = ex.get_traceback_details()
+        except Exception as e:
+            print("--------->", e)
+
+        return make_response(jsonify(response_data)), response_data['status_code']
+
+      
         
         
 
 
 photographer_image_api = PhotographerPortfolioImagesApiView.as_view('photographer_image_api')
+photographer_video_api = PhotographerPortfolioVideoApiView.as_view('photographer_video_api')
 
 photographer_blueprint.add_url_rule(
     '/add_images/',
     view_func=photographer_image_api,
+    methods = ['POST']
+)
+
+photographer_blueprint.add_url_rule(
+    '/add_videos/',
+    view_func=photographer_video_api,
     methods = ['POST']
 )
